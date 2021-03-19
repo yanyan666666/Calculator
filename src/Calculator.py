f1b44afc3333c056c88b74e189d1137acb16929f
yanyan666666 <yy5@njit.edu>
2021-03-19 16:49:03 -0400

    Task: Add the addition method, function and test in calculator and test file.

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,25 +1,10 @@
 from CsvReader import CsvReader
 
-
-def addition(a, b):
-    c = a + b
-    return c
-
-
-def subtraction(a, b):
-    a = int(a)
-    b = int(b)
-    c = b - a
-    return c
-
-
-def mean(data):
-    mean = data
-    return mean
-
+def addition(a,b):
+     return (int)a+(int)b
 
 class Calculator:
-    result = 0
+
 
     def __init__(self):
         pass
@@ -27,18 +12,3 @@ class Calculator:
     def add(self, a, b):
         self.result = addition(a, b)
         return self.result
-
-    def subtract(self, a, b):
-        self.result = subtraction(a, b)
-        return self.result
-
-
-class CSVStats(Calculator):
-    data = []
-
-    def __init__(self, data_file):
-        self.data = CsvReader(data_file)
-        pass
-
-    def mean(self):
-        mean(self.data)
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -5,9 +5,17 @@ from pprint import pprint
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self)->None:
+        self.calculator=Calculator()
 
     def test_instantiate_calculator(self):
-        self.assertIsInstance(self.Calculator, Calculator)
+        self.assertIsInstance(self.calculator, Calculator)
+    def test_results_property_calculator(self):
+        self.assertEqual(self.calculator.result,0)
+
+    def test_add_method_calculator(self):
+        self.assertEqual(self.calculator.add(2,2),4)
+        self.assertEqual(self.calculator.result,4)
 
 
 
