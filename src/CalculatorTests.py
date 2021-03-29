dcf163244bf50a6eb382f3306cf4fc04597fc5e9
yanyan666666 <yy5@njit.edu>
2021-03-28 22:01:39 -0400

    Feature: create and add test methods in CalculatorTests.py.py

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,24 +1,44 @@
 import unittest
 from Calculator import Calculator
-# from CsvReader import CsvReader
-from pprint import pprint
+
+
 
 
 class MyTestCase(unittest.TestCase):
-    def setUp(self)->None:
-        self.calculator=Calculator()
+    def setUp(self) ->None:
+        self.calculator =Calculator()
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
+
     def test_results_property_calculator(self):
-        self.assertEqual(self.calculator.result,0)
+        self.assertEqual(self.calculator.result, 0)
 
     def test_add_method_calculator(self):
-        self.assertEqual(self.calculator.add(2,2),4)
-        self.assertEqual(self.calculator.result,4)
+        self.assertEqual(self.calculator.add(2, 2), 4)
+        self.assertEqual(self.calculator.result, 4)
+
+    def test_subtract_method_calculator(self):
+        self.assertEqual(self.calculator.subtract(2, 2), 0)
+        self.assertEqual(self.calculator.result, 0)
+
+    def test_multiply_method_calculator(self):
+        self.assertEqual(self.calculator.multiply(2, 2), 4)
+        self.assertEqual(self.calculator.result, 4)
+
+    def test_divide_method_calculator(self):
+        self.assertEqual(self.calculator.divide(2, 2), 1)
+        self.assertEqual(self.calculator.result, 1)
+
+    def test_square_method_calculator(self):
+        self.assertEqual(self.calculator.square(2), 4)
+        self.assertEqual(self.calculator.result, 4)
+
+    def test_square_root_method_calculator(self):
+        self.assertEqual(self.calculator.square_root(4), 2)
+        self.assertEqual(self.calculator.result, 2)
 
 
 
-
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
